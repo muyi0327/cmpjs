517486be73b28f4e52d4c9de392c566a49fd1dd8
yangfei <yangfei@ymatou.com>
2016-03-07 13:59:01 +0800

    cc

--- index.js
+++ index.js
@@ -1,10 +1,11 @@
 #!/usr/bin/env node
 
 var program = require('commander');
+var parse5 = require('parse5');
 
 program
 .version('1.0.0')
-.option('--f, format, set export format')
+.option('-f, --format, set export format')
 .parse(process.argv);
 
 if (program.format == 'amd'){

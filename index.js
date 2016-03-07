080f1f79a2f3c7a5e947f83c2c5ae5bca324a4ff
yangfei <yangfei@ymatou.com>
2016-03-07 18:02:03 +0800

    添加init及模块转换

--- index.js
+++ index.js
@@ -2,12 +2,34 @@
 
 var program = require('commander');
 var parse5 = require('parse5');
+var fs = require('fs');
+var path = require('path');
+var umdify = require('umdify');
 
 program
-.version('1.0.0')
-.option('-f, --format, set export format')
-.parse(process.argv);
+    .version('1.0.0')
+    .allowUnknownOption()
+    .option('-f, --format [value]', 'set export format','all')
+    .option('-c, --config [value]', 'set config file path', './cmp.config.js')
+    .option('-b, --build [value]', 'set config file path', './example/test.js');
 
-if (program.format == 'amd'){
-    console.log('export file format type is amd');
+program.parse(process.argv);
+
+if (program.format) {
+    console.log('export file format type is ' + program.format);
 }
+
+if (program.config) {
+    console.log('set config file path ' + program.config);
+}
+
+if(program.build){
+    example(program.build);
+}
+
+function example(dir){
+    var str = fs.readFileSync(dir, 'utf8');
+
+    console.log(umdify(str));
+}
+console.log('program:'+program.format);
---
+++ src/init.js
@@ -0,0 +1,6 @@
+/**
+ *创建cmp.config.js文件
+ */
+module.exports = function(){
+
+}

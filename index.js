53cad76d713f13fe7e51d4998212c7f809906a76
yangfei <yangfei@ymatou.com>
2016-03-07 12:14:51 +0800

    添加命令

--- index.js
+++ index.js
@@ -1,1 +1,12 @@
 #!/usr/bin/env node
+
+var program = require('commander');
+
+program
+.version('1.0.0')
+.option('--f, format, set export format')
+.parse(process.argv);
+
+if (program.format == 'amd'){
+    console.log('export file format type is amd');
+}

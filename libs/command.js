e05abfcf2b75e38f011fcc0cf0cb0f28b3869604
yangfei <yangfei@ymatou.com>
2016-03-31 18:34:18 +0800

    command create

--- libs/command.js
+++ libs/command.js
@@ -1,18 +1,21 @@
 "use strict";
 var inquirer = require("inquirer");
+var nexpect = require('nexpect');
 var path = require('path');
 var fs = require('fs');
 var output = require('./output');
 var baseDir = process.cwd();
 var conf = require('./config');
 var defaultConfigPath = './cmp.config.js';
+var exec = require('child_process').exec;
 
 /**
  * regist commands
  **/
-exports.regist = function(program) {
+exports.regist = function (program) {
     build(program);
     init(program);
+    create(program);
 }
 
 /**
@@ -25,12 +28,12 @@ function build(program) {
         .description('run build commands for components')
         .option("-f, --format [mode]", "Which setup mode to use")
         .option("-c, --config [path]", "config file path")
-        .action(function(entry, options) {
+        .action(function (entry, options) {
             console.log('build file start..');
             options = options || {};
             var format = options.format,
                 configSet = {},
-                configFilePath = path.join(baseDir,options.config || defaultConfigPath);
+                configFilePath = path.join(baseDir, options.config || defaultConfigPath);
 
             if (!fs.existsSync(configFilePath)) {
                 return console.log(
@@ -54,19 +57,18 @@ function build(program) {
 /**
  *create config file
  *@param program {Object} Commander Class
- *@param pkg {Object} npm package.json
  **/
 function init(program) {
     return program
         .command('init [filename]')
         .description('Create component configuration file')
-        .action(function(filename) {
+        .action(function (filename) {
             filename = filename || './cmp.config.js';
             var questions = [{
                 type: "input",
                 name: "name",
                 message: "Please input component name?",
-                validate: function(val) {
+                validate: function (val) {
                     var pass = /^[a-zA-Z]+[a-zA-Z0-9_-]*$/g.test(String(val));
                     if (!val || !pass) {
                         return 'Please enter a valid component name'
@@ -78,7 +80,7 @@ function init(program) {
                 type: "input",
                 name: "entry",
                 message: "\n\nPlease enter the name of the component entry file?",
-                default: function() {
+                default: function () {
                     return './index.cmp';
                 }
             }, {
@@ -90,7 +92,7 @@ function init(program) {
                 type: "input",
                 name: "dest",
                 message: "\n\nEnter the target path for the component file?",
-                default: function() {
+                default: function () {
                     return './dist';
                 }
             }, {
@@ -98,7 +100,7 @@ function init(program) {
                 name: "format",
                 message: "\n\nPlease enter the format of the component?",
                 choices: ["all"].concat(conf.format),
-                default: function() {
+                default: function () {
                     return 'all'
                 }
             }, {
@@ -108,7 +110,7 @@ function init(program) {
                 default: true
             }];
 
-            inquirer.prompt(questions, function(answers) {
+            inquirer.prompt(questions, function (answers) {
                 if (!answers.confirm) {
                     return console.log('Aborting');
                 }
@@ -118,5 +120,22 @@ function init(program) {
                 answers.filename = filename;
                 output.createConfig(answers);
             });
+        });
+}
+
+
+function create(program) {
+    program
+        .command('create [entry]')
+        .description('Create component configuration file')
+        .action(function (entry) {
+            exec('npm init', function (err, stdout, stderr) {
+                if (err) {
+                    return console.log(err);
+                }
+
+                console.log(stdout)
+                console.log(stderr)
+            })
         });
 }
